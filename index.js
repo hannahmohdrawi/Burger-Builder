8bd87b6964e81134a88f053401919fac11e69d2a
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-06-05 12:31:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let radioButtons = document.getElementsByClassName('radioBtn');
+let radios = document.querySelectorAll('input[type="radio"]');
+
+function reset(){
+    let resetBtn = document.getElementsByClassName('resetBtn');
+    radios.forEach((radio => {
+        radio.checked = false;
+    }))
+}
+
+function submitOrder(){
+    let order = [];
+    radios.forEach(radio => {
+        if (radio.checked){
+            order.push(radio.nextElementSibling.textContent)
+        }
+    });
+    document.getElementById('order-info').textContent = order;
+
+}
+    
